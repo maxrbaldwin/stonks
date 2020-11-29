2267d4264f29bcfd977b0a5d130f74192b6ad4fb
maxrbaldwin <maxrbaldwin2328@gmail.com>
2020-11-29 12:52:51 -0500

    clean this up for positions

--- app/modules/fetchPositionQuotes.js
+++ app/modules/fetchPositionQuotes.js
@@ -3,8 +3,33 @@ const { fetchDataByWorksheetIndex, fetchRows } = require('@controllers/spreadshe
 
 const positionsSheetIndex  = 0;
 
-const getPercentChange = (high, low) => `${((high - low) / high) * 100}%`;
-const parseNumber = number => typeof number === 'string' && number.substring(0,1) === '$' ? parseFloat(number.replace('$', '')) : parseFloat(number); 
+const getTenPercentChange = (position, volume) => {
+  const tenPercent = 10/100;
+  const tenPercentChange = ((position * tenPercent) + position) * volume;
+  const totalInvestment = position * volume;
+  return tenPercentChange - totalInvestment;
+}
+
+const getFivePercentLoss = (position, volume) => {
+  const negativeFivePercent = (5/100);
+  const negativeFivePercentChange = ((position * negativeFivePercent) + position) * volume;
+  const totalInvestment = position * volume;
+  return totalInvestment - negativeFivePercentChange;
+}
+
+const getLimitOrder = position => {
+  const tenPercent = 10/100;
+  const tenPercentOf = position * tenPercent;
+  return position + tenPercentOf;
+}
+
+const getStopLoss = position => {
+  const negativeFivePercent = (5/100) * -1;
+  const negativeFivePercentOf = position * negativeFivePercent
+  return position + negativeFivePercentOf
+}
+
+const parseNumber = number => typeof number === 'string' && number.substring(0,1) === '$' ? parseFloat(number.replace('$', '')) : parseFloat(number);
 
 async function fetchPositionQuotes() {
   try {
@@ -15,20 +40,20 @@ async function fetchPositionQuotes() {
     positionsRows.forEach(async row => {
       const { symbol } = row;
       const quote = quotes[symbol];
-      const { openPrice, highPrice, lowPrice } = quote;
-      const buyAt = parseNumber(row['buy at'] || 0);
-      const sellAt = parseNumber(row['sell at'] || 0);
+      const { askPrice, openPrice, highPrice, lowPrice } = quote || {};
+      const position = parseNumber(row.position)
 
+      row.position = position || askPrice;
+      row.volume = row.volume || 30;
+      row.ask = askPrice;
       row.open = openPrice;
       row.high = highPrice;
       row.low = lowPrice;
-      row['high/low % diff'] = getPercentChange(highPrice, lowPrice);
-      row.volume = row.volume || 10;
-      row['calculated goal'] = (sellAt - buyAt) * row.volume
-      row['buy at'] = buyAt
-      row['sell at'] = sellAt
-      row['goal % change'] = getPercentChange(sellAt, buyAt);
-      row['total investment'] = buyAt * row.volume;
+      row['ten percent gain'] = getTenPercentChange(position, row.volume);
+      row['five percent loss'] = getFivePercentLoss (position, row.volume);
+      row['limit order'] = getLimitOrder(position)
+      row['stop loss'] = getStopLoss(position)
+      row['total investment'] = position * row.volume;
       
       await row.save()
     })
